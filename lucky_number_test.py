bfa9ca45dae033dcc1d92ff9f2b92d1d866118de
Igor Sohroin <36764087+anuksen@users.noreply.github.com>
2019-12-30 11:12:19 +0300

    Add files via upload

---
+++ lucky_number_test.py
@@ -0,0 +1,25 @@
+import unittest
+import lucky_number
+
+
+# Тестирование функции, оценивающей равенство половин числа
+class PartsOfNumberTests(unittest.TestCase):
+
+    # Этот тест должен привести к равенству
+    def test_addition_equal(self):
+        t_array = [0, 2, 9, 4, 3, 4]
+
+        result = lucky_number.addition(t_array)
+
+        self.assertEqual(result, True)
+
+    # А этот тест - к неравенству
+    def test_addition_not_equal(self):
+        t_array = [6, 1, 0, 5, 8, 1]
+
+        result = lucky_number.addition(t_array)
+
+        self.assertEqual(result, False)
+
+if __name__ == '__main__':
+    unittest.main()
